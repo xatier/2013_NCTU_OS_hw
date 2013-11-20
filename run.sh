a2fc084bcc6c3ac02c6052f2ae46edfbbabdfa63
xatier <xatierlike@gmail.com>
2013-11-20 11:25:44 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "single"
+g++ -O3 -DSINGLE ./shared_mem.cpp -lrt
+./a.out
+
+echo "copy"
+g++ -O3 -DCOPY ./shared_mem.cpp -lrt
+./a.out
+
+echo "shm"
+g++ -O3 -DSHM ./shared_mem.cpp -rt
+./a.out
+
+echo "mmap"
+g++ -O3 -DMMAP ./shared_mem.cpp -lrt
+./a.out
+
